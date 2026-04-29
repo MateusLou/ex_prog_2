04fdb52403a6705a0ff141e78b57594e7818afea
MateusLou <loureiromateus6@gmail.com>
2026-04-29 08:35:56 -0300

    ex2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,8 @@ def rolar_dados(n):
     for i in range(n):
         dados.append(random.randint(1, 6))
     return dados
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    valor = dados_rolados.pop(dado_para_guardar)
+    dados_no_estoque.append(valor)
+    return [dados_rolados, dados_no_estoque]
